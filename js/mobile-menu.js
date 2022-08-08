b201d91cbb6716ea944dd3df7bf726d13f56ce99
goodcat46 <goodcat1994@gmail.com>
2022-08-08 13:54:40 +0300

    1.006
    - зображення
    - іконки у картах працівиків
    - закриття моб меню
    - стилі моб меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-mobile-menu-open]"),
+    closeModalBtn: document.querySelector("[data-mobile-menu-close]"),
+    modal: document.querySelector("[data-mobile-menu]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
